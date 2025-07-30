86e3cdd0347dd9801b117721fc1f6086f1760c92
Duong Pham <phamanhduong1@dtu.edu.vn>
2025-07-31 01:06:53 +0700

    Problem solved

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(A, ...args) {
+    const result = [];
+    for (let i = 0; i < A.length; i++) {
+        if (!args.includes(A[i])) {
+            result.push(A[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let total = 0;
+    for(let i = a; i <= b; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
